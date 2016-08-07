54ed1794ab4879d1015700d1398df3d5daff9a06
Alexandr Popolitov <popolit@gmail.com>
2016-08-07 19:18:42 +0200

    Switching to QRectFs makes everything nest properly

--- pictures-with-holes.py
+++ pictures-with-holes.py
@@ -4,7 +4,7 @@ import sys
 from PyQt4.QtGui import (QPalette, QWidget, QApplication, QPainter, QColor, QCursor, QImage)
 from PyQt4 import QtCore
 from PyQt4 import Qt
-from PyQt4.QtCore import QRect
+from PyQt4.QtCore import QRect, QRectF
 
 from os import walk
 import os
@@ -20,22 +20,20 @@ THE_Y = 0
 MIN_HOLE_RATIO = 2
 FULL_HOLE_RATIO = 3
 
-MAGIC_RECT = QRect(250,120,50,50)
-
 def linear_transform(src, dst):
     zoom_x = float(dst.width())/src.width()
     zoom_y = float(dst.height())/src.height()
     
     return (lambda rect:
-            QRect(dst.x() + float(rect.x() - src.x()) * zoom_x,
-                  dst.y() + float(rect.y() - src.y()) * zoom_y,
-                  rect.width() * zoom_x,
-                  rect.height() * zoom_y))
+            QRectF(dst.x() + float(rect.x() - src.x()) * zoom_x,
+                   dst.y() + float(rect.y() - src.y()) * zoom_y,
+                   rect.width() * zoom_x,
+                   rect.height() * zoom_y))
 
 
 
 def child_opacity(ratio):
-    return min(float(ratio - MIN_HOLE_RATIO)/(FULL_HOLE_RATIO - MIN_HOLE_RATIO),
+    return min(float(4 * ratio - MIN_HOLE_RATIO)/(FULL_HOLE_RATIO - MIN_HOLE_RATIO),
                1.0)
 
 def parent_opacity(ratio):
@@ -72,17 +70,18 @@ class PictureWithHole(object):
                                                         PDF_BASE_RESOLUTION)
         
         # at first we assume we are rendering the whole of the page
-        self.part = QRect(0, 0,
-                          self.image.width(), self.image.height())
+        self.part = QRectF(0, 0,
+                           self.image.width(), self.image.height())
         self.dest = None
         self.render_ratio = None
         
         self.init_hole_coordinates()
+        print "Loaded:", fname, self.hole.x(), self.hole.y(), self.hole.width(), self.hole.height()
 
     def init_hole_coordinates(self):
         w = self.image.width()
         h = self.image.height()
-        self.hole = QRect(random.random() * 3/4 * w, random.random() * 3/4 * h, 1/4 * w, 1/4 * h)
+        self.hole = QRectF(random.random() * 3/4 * w, random.random() * 3/4 * h, 1.0/4 * w, 1.0/4 * h)
 
     def hole_visible_p(self):
         if self.render_ratio > MIN_HOLE_RATIO:
@@ -98,33 +97,46 @@ class PictureWithHole(object):
         else:
             self.child_pic = None    
 
-    def recursive_draw(self, image):
+    def recursive_draw(self, image, toplevel=True):
         painter = QPainter()
         painter.begin(image)
-        self.draw(painter)
+        self.draw(painter, toplevel=toplevel)
         painter.end()
         if self.child_pic is not None:
-            self.child_pic.recursive_draw(image)
+            print "Drawing childpic"
+            self.child_pic.recursive_draw(image, toplevel=False)
 
-    def draw(self, painter):
-        painter.setOpacity(child_opacity(self.render_ratio))
-        painter.drawImage(self.dest, self.image, self.part)
+    def draw(self, painter, toplevel=True):
+        if toplevel:
+            painter.setOpacity(1.0)
+        else:
+            painter.setOpacity(child_opacity(self.render_ratio))
+        print "Painter opacity:", painter.opacity(), self.render_ratio
+        painter.drawImage(self.dest.toAlignedRect(), self.image, self.part.toAlignedRect())
+
+        # mark, where the hole is
+        print "Hole:", self.hole, linear_transform(self.part, self.dest)(self.hole)
+        oldOpacity = painter.opacity()
+        painter.setPen(QtCore.Qt.black)
+        painter.drawRect(linear_transform(self.part, self.dest)(self.hole))
+        painter.setOpacity(oldOpacity)
+        
         it = self.hole_visible_p()
         if it:
             its_dst = linear_transform(self.part, self.dest)(it)
             painter.setOpacity(1.0)
             painter.fillRect(its_dst, QtCore.Qt.white)
             painter.setOpacity(parent_opacity(self.render_ratio))
-            painter.drawImage(self.dst_it, self.image, it)
+            painter.drawImage(its_dst.toAlignedRect(), self.image, it.toAlignedRect())
 
     def hole_takes_whole_qimage(self):
         return self.hole.contains(self.part)
     def takes_whole_qimage(self, image):
-        return self.dest.contains(image.rect())
+        return self.dest.contains(QRectF(image.rect()))
 
     def glue_on_top(self, new_pic, image):
         new_pic.child_pic = self
-        new_pic.dest = image.rect()
+        new_pic.dest = QRectF(image.rect())
 
         # determine coordinates of the visible part of the hole in its parent coord system
         top = new_pic.hole.top() + 1.0/4 * self.part.top()
@@ -132,7 +144,7 @@ class PictureWithHole(object):
         left = new_pic.hole.left() + 1.0/4 * self.part.left()
         right = new_pic.hole.left() + 1.0/4 * self.part.right()
 
-        new_pic.part = linear_transform(self.dest, QRect(left, top, right-left, bottom-top))(self.dest)
+        new_pic.part = linear_transform(self.dest, QRectF(left, top, right-left, bottom-top))(self.dest)
         
         new_pic.determine_render_ratio()
         return new_pic
@@ -149,28 +161,28 @@ class PictureWithHole(object):
 
     def recursive_rescale_and_sync(self, x_m, y_m, zoom=1.0, image=None):
         # we rescale destination
-        self.dest = QRect((self.dest.x() - x_m) * zoom + x_m,
-                          (self.dest.y() - y_m) * zoom + y_m,
-                          self.dest.width() * zoom,
-                          self.dest.height() * zoom)
+        self.dest = QRectF(float(self.dest.x() - x_m) * zoom + x_m,
+                           float(self.dest.y() - y_m) * zoom + y_m,
+                           self.dest.width() * zoom,
+                           self.dest.height() * zoom)
 
         self.determine_render_ratio()
 
-        # we crop part and destination if they take more than qimage
-        if self.dest.x() < 0:
-            self.part.setLeft(self.part.x() - float(self.dest.x()) / self.render_ratio)
-            self.dest.setLeft(0)
-        if self.dest.y() < 0:
-            self.part.setTop(self.part.y() - float(self.dest.y()) / self.render_ratio)
-            self.dest.setTop(0)
-        if self.dest.right() > image.rect().right():
-            self.part.setRight(self.part.right()
-                               - float(self.dest.right()-image.rect().right())/self.render_ratio)
-            self.dest.setRight(image.rect().right())
-        if self.dest.bottom() > image.rect().bottom():
-            self.part.setBottom(self.part.bottom()
-                                - float(self.dest.bottom()-image.rect().bottom())/self.render_ratio)
-            self.dest.setBottom(image.rect().bottom())
+        # # we crop part and destination if they take more than qimage
+        # if self.dest.x() < 0:
+        #     self.part.setLeft(self.part.x() - float(self.dest.x()) / self.render_ratio)
+        #     self.dest.setLeft(0)
+        # if self.dest.y() < 0:
+        #     self.part.setTop(self.part.y() - float(self.dest.y()) / self.render_ratio)
+        #     self.dest.setTop(0)
+        # if self.dest.right() > image.rect().right():
+        #     self.part.setRight(self.part.right()
+        #                        - float(self.dest.right()-image.rect().right())/self.render_ratio)
+        #     self.dest.setRight(image.rect().right())
+        # if self.dest.bottom() > image.rect().bottom():
+        #     self.part.setBottom(self.part.bottom()
+        #                         - float(self.dest.bottom()-image.rect().bottom())/self.render_ratio)
+        #     self.dest.setBottom(image.rect().bottom())
         
         # we sync and recurse on a child
         self.sync_child_pic()
@@ -201,9 +213,12 @@ class PicturesWithHolesWidget(QWidget):
         
         self.init_pdf_image_geometry()
 
-        self.refresh_the_qimage()
+        print self.pics.part, self.pics.dest, self.width(), self.height()
+        
+        self.refresh_the_qimage() # color=QtCore.Qt.transparent)
         self.pics.recursive_draw(self.the_qimage)
-
+        self.update()
+        
     def init_pdf_image_geometry(self):
         my_height = self.height()
         my_width = self.width()
@@ -211,10 +226,10 @@ class PicturesWithHolesWidget(QWidget):
         ratio = max(float(self.pics.image.width())/my_width,
                     float(self.pics.image.height())/my_height)
 
-        self.the_qimage = QImage(self.pics.image.width() * ratio,
-                                 self.pics.image.height() * ratio,
+        self.the_qimage = QImage(float(self.pics.image.width()) / ratio,
+                                 float(self.pics.image.height()) / ratio,
                                  self.pics.image.format())
-        self.pics.dest = self.the_qimage.rect()
+        self.pics.dest = QRectF(self.the_qimage.rect())
         self.pics.determine_render_ratio()
         
     def keyPressEvent(self, event):
@@ -227,8 +242,10 @@ class PicturesWithHolesWidget(QWidget):
         y_image = float(self.height() - self.the_qimage.height())/2
         x_m = pos.x() - x_image
         y_m = pos.y() - y_image
+        zoom = (1.0 + 0.1 * float(event.delta())/8/15/20)
+        print "Zoom:", zoom
         self.pics.recursive_rescale_and_sync(x_m, y_m,
-                                             zoom = (1.0 + 0.1 * float(event.delta())/8/15/20),
+                                             zoom = zoom,
                                              image = self.the_qimage)
         if not self.pics.takes_whole_qimage(self.the_qimage):
             self.pics = self.pics.glue_on_top(PictureWithHole(random_sketches_fname()),
@@ -241,8 +258,8 @@ class PicturesWithHolesWidget(QWidget):
         self.pics.recursive_draw(self.the_qimage)
         self.update()
 
-    def refresh_the_qimage(self):
-        self.the_qimage.fill(QtCore.Qt.white)
+    def refresh_the_qimage(self, color=QtCore.Qt.white):
+        self.the_qimage.fill(color)
         
     def paintEvent(self, event):
         x = (self.frameSize().width() - self.the_qimage.width())/2
