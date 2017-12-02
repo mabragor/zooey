80639898ccea01424dffd155e0df2ae388db93ee
Alexander Popolitov <popolit@gmail.com>
2017-12-03 00:20:09 +0100

    Select active object

--- new-colored-boxes.py
+++ new-colored-boxes.py
@@ -13,13 +13,17 @@ from random import randint
 
 
 class ColoredCircle(object):
-    def __init__(self, x, y, r=10):
+    def __init__(self, x, y, r=100):
         self.center = QPoint(x, y)
         self.radius = r
         self.color = QtGui.QColor(randint(0, 255),
                                   randint(0, 255),
                                   randint(0, 255))
 
+    def contains(self, x, y):
+        return (pow(self.center.x() - x, 2) + pow(self.center.y() - y, 2)
+                < pow(self.radius, 2))
+        
     def draw(self, widget):
         p = QPainter(widget)
         p.setBrush(QtGui.QBrush(self.color))
@@ -27,13 +31,20 @@ class ColoredCircle(object):
 
         
 class ColoredSquare(object):
-    def __init__(self, x, y, half_width=10):
+    def __init__(self, x, y, half_width=100):
         self.center = QPoint(x, y)
         self.half_width = half_width
         self.color = QtGui.QColor(randint(0, 255),
                                   randint(0, 255),
                                   randint(0, 255))
 
+    def contains(self, x, y):
+        return ((self.center.x() - self.half_width < x
+                 and self.center.x() + self.half_width > x)
+                and
+                (self.center.y() - self.half_width < y
+                 and self.center.y() + self.half_width > y))
+        
     def draw(self, widget):
         p = QPainter(widget)
         p.setBrush(QtGui.QBrush(self.color))
@@ -55,6 +66,7 @@ class NewColoredBoxes(QWidget):
 
         # ### The objects (contained) in the current widget
         self.objects = []
+        self.active_object = None
         
         self.hide()
 
@@ -71,11 +83,34 @@ class NewColoredBoxes(QWidget):
             self.stop()
 
     def mousePressEvent(self, event):
-        if event.button() == QtCore.Qt.LeftButton:
-            self.add_circle_at_point(event.x(), event.y())
-        elif event.button() == QtCore.Qt.RightButton:
-            self.add_square_at_point(event.x(), event.y())
-        self.update()
+        if event.modifiers() == QtCore.Qt.ControlModifier:
+            # ### We create boxes or circles when Ctrl is pressed
+            if event.button() == QtCore.Qt.LeftButton:
+                self.add_circle_at_point(event.x(), event.y())
+            elif event.button() == QtCore.Qt.RightButton:
+                self.add_square_at_point(event.x(), event.y())
+            self.update()
+        elif event.modifiers() == QtCore.Qt.NoModifier:
+            if event.button() == QtCore.Qt.LeftButton:
+                self.try_find_active_object(event)
+
+    def mouseReleaseEvent(self, event):
+        if self.active_object is not None:
+            self.objects.append(self.active_object)
+            self.active_object = None
+                
+    def find_object_at_point(self, x, y):
+        for obj in reversed(self.objects):
+            if obj.contains(x, y):
+                return obj
+        return None
+                
+    def try_find_active_object(self, event):
+        it = self.find_object_at_point(event.x(), event.y())
+        if it is not None:
+            self.active_object = it
+            self.objects.remove(it)
+            self.update()
 
     def add_circle_at_point(self, x, y):
         self.objects.append(ColoredCircle(x, y))
@@ -86,6 +121,8 @@ class NewColoredBoxes(QWidget):
     def paintEvent(self, event):
         for obj in self.objects:
             obj.draw(self)
+        if self.active_object is not None:
+            self.active_object.draw(self)
         
         
 if __name__ == '__main__':
